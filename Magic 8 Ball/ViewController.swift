57a44fc6db8ded9b4d650026b99ae47904e36e34
Mitch Davis <mitchelldavis44@gmail.com>
2018-02-24 20:50:44 -0600

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Mitchell Davis on 2/24/18.
+//  Copyright © 2018 Mitchell Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
